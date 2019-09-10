6c3ecf2a7253b81a7692a312ed8f72c9fc424d76
Brain_Fry <Syedfairozpasha@gmail.com>
2019-09-10 12:31:52 +0530

    Completed job model

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Job
+
 # Create your views here.
 def HomePageHandler(request):
-    return render(request,'jobs/HomePage.html')
+
+    jobs = Job.objects
+    return render(request,'jobs/HomePage.html',{"jobs":jobs})
